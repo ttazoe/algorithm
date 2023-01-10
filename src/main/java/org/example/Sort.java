330f66a0894a79add958eecf41b1599febb4a1b1
tatazoe <takashi.tazoe@gmail.com>
2023-01-10 21:28:49 +0900

    :add: Bubble sort

--- src/main/java/org/example/Sort.java
+++ src/main/java/org/example/Sort.java
@@ -1,8 +1,8 @@
 package org.example;
 
 public class Sort {
-    public static int[] insertionSort(int[] list){
-        for (int i = 1; i < list.length; i++){
+    public int[] insertionSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
             int target = list[i];
 
             int j = i - 1;
@@ -11,11 +11,33 @@ public class Sort {
                 j--;
             }
             list[j + 1] = target;
-            for (int tmp = 0; tmp < list.length; tmp++) {
-                System.out.printf("%d ", list[tmp]);
-            }
-            System.out.println("");
+
+            printList(list);
         }
         return list;
+    }
+
+    public int[] bubbleSort(int[] list) {
+        int unsortedIndex = list.length;
+        while (unsortedIndex > 0) {
+            for (int i = 0; i < unsortedIndex - 1; i++) {
+                if (list[i] > list[i + 1]) {
+                    int tmp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = tmp;
+                }
+            }
+            unsortedIndex--;
+            printList(list);
+        }
+        return list;
+    }
+
+
+    void printList (int[] list) {
+        for (int tmp = 0; tmp < list.length; tmp++) {
+            System.out.printf("%d ", list[tmp]);
+        }
+        System.out.println("");
     }
 }
--- src/test/java/org/example/SortTest.java
+++ src/test/java/org/example/SortTest.java
@@ -7,7 +7,15 @@ class SortTest {
     @Test
     public void testInsertionSort(){
         int[] list = {5, 2, 4, 6, 1 ,3};
-        int[] sortedList = Sort.insertionSort(list);
+        Sort sort = new Sort();
+        int[] sortedList = sort.insertionSort(list);
+    }
+
+    @Test
+    public void testBubbleSort(){
+        int[] list = {5, 2, 4, 6, 1 ,3};
+        Sort sort = new Sort();
+        int[] sortedList = sort.bubbleSort(list);
     }
 
 
