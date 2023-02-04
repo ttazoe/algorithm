b5056a8068944bcee56ba79c0f0c5597e30b8a9f
tatazoe <takashi.tazoe@gmail.com>
2023-02-04 18:17:12 +0900

    :update: refactor binary search

--- src/main/java/org/example/search/binary/BinarySearch.java
+++ src/main/java/org/example/search/binary/BinarySearch.java
@@ -3,14 +3,13 @@ package org.example.search.binary;
 public class BinarySearch {
     static boolean search(int[] list, int x) {
         int first = 0;
-        int last = list.length -1 ;
+        int last = list.length;
 
-        while (true) {
+        while (first < last) {
             int mid = (first + last) / 2;
             System.out.printf("%d %d %d \n", first, last, mid);
-            if (list[mid] == x || list[first] == x || list[last] == x) return  true;
-            if (mid == first || mid == last) break;
-            if (list[mid] < x) first = mid;
+            if (list[mid] == x) return  true;
+            if (list[mid] < x) first = mid + 1;
             if (list[mid] > x) last = mid;
         }
         return false;
