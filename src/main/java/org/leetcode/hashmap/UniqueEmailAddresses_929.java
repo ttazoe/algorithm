f2a7de2cdeaf1d4cbde910459fda203d5640b046
tatazoe <takashi.tazoe@gmail.com>
2023-02-07 09:06:46 +0900

    :add: leet code 929

---
+++ src/main/java/org/leetcode/hashmap/UniqueEmailAddresses_929.java
@@ -0,0 +1,40 @@
+package org.leetcode.hashmap;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueEmailAddresses_929 {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> uniqueEmailSet = new HashSet<>();
+
+        for(String email : emails) {
+            int atIndex = email.indexOf("@");
+            int plusIndex = email.indexOf("+");
+
+            String local;
+            String domain = email.substring(atIndex);
+            if(plusIndex > 0 && plusIndex < atIndex) {
+                local = email.substring(0,plusIndex).replace(".","");
+            } else {
+                local = email.substring(0,atIndex).replace(".","");
+            }
+            uniqueEmailSet.add(local + domain);
+        }
+        return uniqueEmailSet.size();
+    }
+
+//    //Original Solution
+//    public int numUniqueEmails(String[] emails) {
+//        Set<String> uniqueEmailSet = new HashSet<>();
+//
+//        for(String email : emails) {
+//            String local = email.split("@")[0];
+//            String domain = email.split("@")[1];
+//            String localFilterByPlus = local.split("\\+")[0];
+//            String localFilterByDot = localFilterByPlus.replace(".","");
+//            String keyEmail = localFilterByDot + "@" + domain;
+//            uniqueEmailSet.add(keyEmail);
+//        }
+//        return uniqueEmailSet.size();
+//    }
+}
