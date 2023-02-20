43fac397740f0e91eae1163fc0bf06115f70b2da
tatazoe <takashi.tazoe@gmail.com>
2023-02-20 16:01:20 +0900

    :add: Leet code 2

---
+++ src/main/java/org/leetcode/linked_list/AddTwoNumbers_2.java
@@ -0,0 +1,36 @@
+package org.leetcode.linked_list;
+
+public class AddTwoNumbers_2 {    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+  ListNode sentinel = new ListNode(0);
+  ListNode head = sentinel;
+  int carry = 0;
+
+  while (l1 != null || l2 != null) {
+    int l1val = 0;
+    int l2val = 0;
+    int sum = 0;
+
+    if (l1 != null) {
+      l1val = l1.val;
+      l1 = l1.next;
+    }
+
+    if (l2 != null) {
+      l2val = l2.val;
+      l2 = l2.next;
+
+    }
+
+    sum = l1val + l2val + carry;
+    carry = sum / 10;
+    head.next = new ListNode(sum % 10);
+    head = head.next;
+  }
+
+  if (carry == 1) {
+    head.next = new ListNode(carry);
+  }
+
+  return sentinel.next;
+}
+}
